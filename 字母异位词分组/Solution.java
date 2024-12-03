9f7193a37946b72c2da23065ac9125200b638ae6
klhu03 <klhu03@mail.yst.com.cn>
2024-12-03 15:06:38 +0800

    12.3 字母异位词分组

---
+++ 字母异位词分组/Solution.java
@@ -0,0 +1,21 @@
+package 字母异位词分组;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Arrays;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for(String str : strs){
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String key = new String(charArray);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
