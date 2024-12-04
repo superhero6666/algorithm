068dacbedf83d6e789fbc9f03b6f2b2ab46cff23
klhu03 <klhu03@mail.yst.com.cn>
2024-12-04 10:12:31 +0800

    12.4 第k个最大数215

---
+++ dao/TreeNode.java
@@ -0,0 +1,18 @@
+package dao;
+
+/**
+ * @author klhu03
+ * @since 2024/12/03 15:44
+ **/
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ 数组中的第k个最大元素215/Solution.java
@@ -0,0 +1,60 @@
+package 数组中的第k个最大元素215;
+
+
+/**
+ * @author klhu03
+ * @since 2024/12/03 15:51
+ **/
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        return maxK(nums, k-1, 0, nums.length - 1);
+    }
+
+    public int maxK(int[] nums, int k, int left, int right) {
+        if( left == right){
+            return nums[k];
+        }
+        int i = left-1, j = right+1;
+        int key = nums[left];
+        while (i < j) {
+            do {
+                i++;
+            } while (nums[i]>key);
+            do {
+                j--;
+            } while (nums[j]<key);
+            if(i < j){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+        if(i < k){
+            return maxK(nums, k, i+1, right);
+        }else{
+            return maxK(nums, k, left, i);
+        }
+    }
+}
+    //超时
+//        Stack<Integer> stack1 = new Stack<>();
+//        Stack<Integer> stack2 = new Stack<>();
+//        for (int i = 0; i < nums.length; i++) {
+//            if(stack1.isEmpty()){
+//                stack1.push(nums[i]);
+//                continue;
+//            }
+//            while(!stack1.isEmpty() && stack1.peek() < nums[i]){
+//                stack2.push(stack1.pop());
+//            }
+//            if(stack1.size() < k){
+//                stack1.push(nums[i]);
+//            }
+//            while(!stack2.isEmpty() && stack1.size() < k){
+//                stack1.push(stack2.pop());
+//            }
+//        }
+//        return stack1.pop();
+//    }
+    //3 2 1 5 6 4
+
